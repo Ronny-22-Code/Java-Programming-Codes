e8af70cfa7f42921b7b7e49fbbd8d0944cbbe3df
Rohan Sethi <mailtorohansethi@gmail.com>
2019-10-19 02:23:22 +0530

    Execution Class for Queue Operations

---
+++ Client_Class.java
@@ -0,0 +1,23 @@
+package java_18_19_20_Oct_2019;
+
+public class Client_Class {
+
+
+	public static void main(String[] args) throws Exception {
+		
+	
+		Queue_Exception Q = new Queue_Exception();
+	
+	
+		Q.Enqueue(5);
+		Q.Enqueue(4);
+		Q.Enqueue(6);
+		Q.Enqueue(7);
+		Q.Enqueue(8);
+		
+		Q.display();
+	
+	}
+
+
+}
