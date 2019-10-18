eabd3a7dc96550b970c0e87aa35d3452e5a15797
Rohan Sethi <mailtorohansethi@gmail.com>
2019-10-19 02:27:15 +0530

    Execution Class for Stack with Exception Handling

---
+++ Stack_New_Client.java
@@ -0,0 +1,24 @@
+package java_18_19_20_Oct_2019;
+
+public class Stack_New_Client {
+
+
+public static void main(String[] args) throws Exception {
+		
+		Stack_Exception S = new Stack_Exception();
+	
+	
+		S.push(5,6);
+		S.push(4,6);
+		S.push(9,6);
+		S.push(8,6);
+		S.push(7,6);
+		
+	
+		S.pop();
+	
+		S.display();
+	
+	}
+
+}
