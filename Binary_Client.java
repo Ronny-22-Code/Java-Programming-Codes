16fb01e62b3daffedf46b2197d5f468192def19a
Rohan Sethi <mailtorohansethi@gmail.com>
2019-11-02 00:30:47 +0530

    Binary Tree Execution
    This source code file executes all the operations of binary tree by the creation of object of binary tree class

---
+++ Binary_Client.java
@@ -0,0 +1,21 @@
+package pack_24_25_26_Oct_2019;
+
+public class Binary_Client {
+
+	public static void main(String[] args) {
+
+		BinaryTree bc = new BinaryTree();
+
+		bc.display();
+
+		bc.size();
+
+		bc.max();
+
+		bc.find(5);
+
+		bc.height();
+
+	}
+
+}
